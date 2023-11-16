215c1e0c1c76e7220f60b0298717dc737513ef31
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-16 12:55:50 -0800

    1.0.2

--- src/Terpz710/BasicHealer/Main.php
+++ src/Terpz710/BasicHealer/Main.php
@@ -1,22 +1,22 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Terpz710\BasicHealer;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-
 use Terpz710\BasicHealer\Commands\FeedCommand;
 use Terpz710\BasicHealer\Commands\HealCommand;
 
 class Main extends PluginBase {
 
-    public function onEnable(): void {
-        $this->saveResource("heal&feed.yml");
-        $this->config = new Config($this->getDataFolder() . "heal&feed.yml", Config::YAML);
+    /** @var Config */
+    private $config;
 
-        $this->getServer()->getCommandMap()->register("feed", new FeedCommand($this));
-        $this->getServer()->getCommandMap()->register("heal", new HealCommand($this));
+    public function onEnable(): void {
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+
+        $this->getServer()->getCommandMap()->register("feed", new FeedCommand($this->config));
+        $this->getServer()->getCommandMap()->register("heal", new HealCommand($this->config));
     }
 }
